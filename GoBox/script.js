e7cd9a1424101aa13687e2d4d1be5d49c10ce3e3
Younous <105168893+YounousTech@users.noreply.github.com>
2022-11-05 17:32:20 +0100

    Add files via upload

--- GoBox/script.js
+++ GoBox/script.js
@@ -28,4 +28,32 @@ window.addEventListener('load',()=>{
     TL.play()
 })
 
+//dark and light mode
 
+var imgmode = document.getElementById("image_bird");
+imgmode.src = "img/GoBox dark.png";
+document.getElementById("théme-btn").innerHTML = '<i class="bi bi-lightbulb"></i>';
+var bg = document.querySelector("body");
+var span1 = document.querySelector(".span1");
+var span2 = document.querySelector(".span2");
+var théme = "dark";
+
+function changetheme(){
+    if(théme == "dark"){
+        théme = "light";
+        imgmode.src = "img/GoBox light.png";
+        bg.style.backgroundColor = "#fff";
+        span1.style.color = "black";
+        span2.style.color = "black";
+        document.getElementById("théme-btn").innerHTML = '<i class="bi bi-moon"></i>';
+        document.querySelector(".left").style.backgroundColor = "#ffffffcc";
+    }else{
+        théme = "dark";
+        imgmode.src = "img/GoBox dark.png";
+        bg.style.backgroundColor = "#000000";
+        span1.style.color = "#fff";
+        span2.style.color = "#fff";
+        document.getElementById("théme-btn").innerHTML = '<i class="bi bi-lightbulb"></i>';
+        document.querySelector(".left").style.backgroundColor = "#000000cc";
+    }
+}
